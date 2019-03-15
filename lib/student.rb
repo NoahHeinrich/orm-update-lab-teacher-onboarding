30e39401b5b5e4e79c414c8b466cac4532d4ab81
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 18:48:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,5 +60,6 @@ class Student
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql,name)
+    Student.new_from_db(result)
   end
 end
