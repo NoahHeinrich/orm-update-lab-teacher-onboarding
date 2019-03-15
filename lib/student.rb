9a0de4134bba504cc32d8dc1699b091b5f8b9a49
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 18:26:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_reader :name, :grade
+  attr_reader :name, :grade, :id
   def initialize(name,grade)
     @name = name
     @grade = grade
